bb051795efea35dfeec8dc5fce393d2980dad28e
braids <cymoro@gmail.com>
2018-01-16 19:12:16 -0800

    EventTimer Created
    - Timer.h
    -- EventTimer class deriving from Timer. Will run for duration specified then stop. Can loop and fire another timer class.

--- TrySomethingNew/Timer.h
+++ TrySomethingNew/Timer.h
@@ -26,4 +26,59 @@ public:
 	bool isPaused();
 };
 
+
+class EventTimer : public Timer {
+protected:
+	EventTimer* NextTimer;
+	Uint32		EventFireTime;
+	bool		Loop;
+
+public:
+	EventTimer() {
+		this->SetNextTimer(nullptr);
+		this->SetEventFireTime(0);
+		this->SetLoop(false);
+	}
+	EventTimer(Uint32 _eventTime) {
+		this->SetNextTimer(nullptr);
+		this->SetEventFireTime(_eventTime);
+		this->SetLoop(false);
+	}
+	EventTimer(Uint32 _eventTime, bool _loop) {
+		this->SetNextTimer(nullptr);
+		this->SetEventFireTime(_eventTime);
+		this->SetLoop(_loop);
+	}
+	EventTimer(Uint32 _eventTime, EventTimer* _next) {
+		this->SetNextTimer(_next);
+		this->SetEventFireTime(_eventTime);
+		this->SetLoop(false);
+	}
+	EventTimer(Uint32 _eventTime, EventTimer* _next, bool _loop) {
+		this->SetNextTimer(_next);
+		this->SetEventFireTime(_eventTime);
+		this->SetLoop(_loop);
+	}
+	
+	void SetNextTimer(EventTimer* _next) { this->NextTimer = _next; }
+	EventTimer* GetNextTimer() { return this->NextTimer; }
+	
+	void SetEventFireTime(Uint32 _eventTime) { this->EventFireTime = _eventTime; }
+	Uint32 GetEventFireTime() { return this->EventFireTime; }
+	
+	bool IsLooping() { return this->Loop; }
+	void SetLoop(bool _loop) { this->Loop = _loop; }
+
+	void Update() {
+		if (this->getTicks() >= this->GetEventFireTime()) {
+			// Stop timer
+			this->stop();
+			// If timer loops, start timer
+			if (this->IsLooping()) this->start();
+			// If another timer starts after this, start the next timer
+			if (this->GetNextTimer() != nullptr)
+				this->GetNextTimer()->start();
+		}
+	}
+};
 #endif
