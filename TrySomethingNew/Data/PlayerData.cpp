8ace2a9443bed26f0b8575cb15dab76ac2321dd7
braids <cymoro@gmail.com>
2018-01-19 16:55:57 -0800

    Removed commented code from PlayerData.cpp

--- TrySomethingNew/Data/PlayerData.cpp
+++ TrySomethingNew/Data/PlayerData.cpp
@@ -17,27 +17,12 @@ void PlayerData::ResetPlayerData() {
 
 ItemData* PlayerData::GetInventoryItem(ItemName _name) {
 	return GetItemFromVector(_name, &this->Inventory);
-	//return *(find_if(this->Inventory.begin(), this->Inventory.end(), [&_name](ItemData* &_item) { return _item->GetName() == _name; }));
 }
 
 bool PlayerData::HasInventoryItem(ItemName _name) {
-	/*
-	ItemData* item = this->GetInventoryItem(_name);
-	if (item == nullptr)
-		return false;
-	return (item->GetQuantity() > 0) ? true : false;
-	*/
 	return (this->GetInventoryItem(_name)->GetQuantity() > 0) ? true : false;
 }
 
 void PlayerData::SetInventoryQty(ItemName _name, int _qty) {
-	/*
-	ItemData* item = this->GetInventoryItem(_name);
-	if (item == nullptr) {
-		this->Inventory.push_back(new ItemData(_name));
-		item = this->GetInventoryItem(_name);
-	}
-	item->SetQuantity(_qty);
-	*/
 	this->GetInventoryItem(_name)->SetQuantity(_qty);
 }
